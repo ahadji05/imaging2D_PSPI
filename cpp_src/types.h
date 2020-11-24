5eaa5f1ffcfe0775689a23a03df3153ed7c72275
ahadji05 <andreash95.AH@gmail.com>
2020-11-24 15:51:42 +0200

    datatypes and headers

---
+++ cpp_src/types.h
@@ -0,0 +1,15 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// #include <thrust/complex.h>
+#include "complex.h"
+
+extern "C"
+{
+
+// typedef thrust::complex<float> fcomp;
+typedef std::complex<float> fcomp;
+
+}
+
+#endif
