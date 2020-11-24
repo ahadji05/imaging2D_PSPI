0c470a717c5d7e35b76e275aa5c27fe3910d6590
ahadji05 <andreash95.AH@gmail.com>
2020-11-24 15:47:04 +0200

    imaging condition based on cross-correlation in the f-x domain

---
+++ cpp_src/imag_condition.cpp
@@ -0,0 +1,34 @@
+
+#include "types.h"
+#include <iostream>
+
+extern "C"
+
+{
+
+void cross_corr(int ns, int shotSize, int nx, int nf, fcomp * forw, fcomp * back, int sizeImage, int depthIdx, float * image){
+
+    fcomp * conv = new fcomp[ns*nx];
+
+    #pragma omp parallel for schedule(dynamic, 1)
+    for (int is=0; is<ns; ++is)
+        for (int i=0; i<nx; i++)
+            conv[is*nx + i] = fcomp(0.0,0.0);
+    
+    #pragma omp parallel for schedule(dynamic, 1)
+    for (int is=0; is<ns; ++is)
+        for (int j=0; j<nf; j++)
+            for (int i=0; i<nx; i++)
+                conv[is*nx + i] += forw[is*shotSize + j*nx + i] * std::conj(back[is*shotSize + j*nx + i]);
+
+    #pragma omp parallel for schedule(dynamic, 1)
+    for (int is=0; is<ns; ++is)
+        for (int i=0; i<nx; i++){
+            image[is*sizeImage + depthIdx*nx + i] = reinterpret_cast<float*>(&conv[is*nx])[2*i];
+        }
+
+    delete [] conv;
+
+}
+
+} // end extern "C"
