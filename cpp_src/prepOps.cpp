abfbbc9eeeb47f99bf8414afc586705952e10ede
ahadji05 <andreash95.AH@gmail.com>
2020-12-16 17:02:22 +0200

    class that hides under constructor the functionality for creating a set/table of propagation operators

---
+++ cpp_src/prepOps.cpp
@@ -0,0 +1,181 @@
+
+#include "prepOps.h"
+
+extern "C"
+
+{
+
+
+// FUNCTION SCOPE: Define N kappa(k) values in an array, in the range [0-kmax].
+// INPUT parameters:
+//    - kmax : max kappa value in the final array ( k[N-1] = kmax )
+//    - N : number of kappa values
+// OUTPUT:
+//    - k : array with the final values (result)
+void create_k(float * k, float kmax, int N){
+
+    float dk = kmax / (N-1);
+
+    for(int i=0; i<N; ++i)
+        k[i] = i*dk;
+}
+
+
+
+// FUNCTION SCOPE:
+
+void read_kx(float * kx, int N, float * read_from){
+
+    for(int i=0; i<N; ++i)
+        kx[i] = read_from[i];
+}
+
+
+
+// FUNCTION SCOPE: Prepare a 2D table of 1D operators for forward-in-time
+//    propagation. Operators are designed based on a set of k's given in array
+//    k, and given a set of wavenumbers (kx).
+// INPUT parameters:
+//    - nk : number of k values
+//    - k : array with desired k values (must: kappa[0] = 0.0, kappa[nk-1]
+// be kmax, no negative values)
+//    - nkx : number of kx values (wavenumbers)
+//    - kx : array with wavenumbers (represent the positive Fourier 
+// coefficients)
+//    - dz : depth step (dz > 0)
+// OUTPUT:
+//    - psOp : "2D" array of ns 1D operators
+void forwOperators(fcomp * psOp, int nk, float * k, int nkx, float * kx, float dz){
+
+    fcomp kz;
+
+    for(int j=0; j<nk; ++j){
+
+        for(int i=0; i<nkx; ++i){
+        
+            if ( std::abs(k[j]) >= std::abs(kx[i]) ){
+        
+                kz = std::sqrt( std::pow(k[j], 2) - std::pow(kx[i], 2) );
+        
+            } else {
+            
+                float term = std::sqrt( std::pow(kx[i],2) - std::pow(k[j], 2) );
+                kz = -fcomp(0.0,1.0) * term;
+            }
+            psOp[j*nkx + i] = std::exp( -fcomp(0.0,1.0) * (kz - k[j]) * dz );
+        }
+    }
+}
+
+
+
+// FUNCTION SCOPE: Prepare a 2D table of 1D operators for backward-in-time
+//    propagation. Operators are designed based on a set of k's given in array
+//    k, and given a set of wavenumbers (kx).
+// INPUT parameters:
+//    - nk : number of k values
+//    - k : array with desired k values (must: kappa[0] = 0.0, kappa[nk-1]
+// be kmax, no negative values)
+//    - nkx : number of kx values (wavenumbers)
+//    - kx : array with wavenumbers (represent the positive Fourier 
+// coefficients)
+//    - dz : depth step (dz > 0)
+// OUTPUT:
+//    - psOp : "2D" array of ns 1D operators
+void backOperators(fcomp * psOp, int nk, float * k, int nkx, float * kx, float dz){
+
+    fcomp kz;
+
+    for(int j=0; j<nk; ++j){
+
+        for(int i=0; i<nkx; ++i){
+        
+            if ( std::abs(k[j]) >= std::abs(kx[i]) ){
+        
+                kz = std::sqrt( std::pow(k[j], 2) - std::pow(kx[i], 2) );
+        
+            } else {
+        
+                float term = std::sqrt( std::pow(kx[i],2) - std::pow(k[j], 2) );
+                kz = +fcomp(0.0,1.0) * term;
+            }
+            psOp[j*nkx + i] = std::exp( +fcomp(0.0,1.0) * (kz - k[j]) * dz );
+        }
+    }
+}
+
+
+
+// FUNCTION SCOPE: select based on indexing the most suitable operator for
+//    propagation from the table of operators (class f_kx_operators.)
+//    assume data in array are equally separated, starting ftom 0.0
+int chooseOperatorIndex(int N, float * k, float kref){
+
+    if(N < 2){
+        std::cout << "too small vector\n";
+        exit(1);
+    }
+    float deltak = k[1] - k[0];
+
+    if (deltak == 0.0 || deltak < 0){
+        std::cout << "The provided kappa array is wrong!\n";
+        exit(1);
+    }
+
+    if(kref < 0.0){
+        std::cout << "kref cannot be negative!\n";
+        exit(1);
+    }
+
+    if(kref > k[N-1]+deltak){
+        std::cout << "kref is larger than the largest k!\n";
+        exit(1);
+    }
+
+    int idx = 0;
+    if (kref == 0)
+        return idx;
+
+    idx = (int)(std::floor( kref / deltak ));
+
+    if (idx > N-2){
+        idx = N-1; //without this, potential for invalid access in next steps!
+        return idx;
+    }
+
+    float diffDown = std::abs(kref - k[idx]);
+    float diffUp = std::abs(kref - k[idx+1]);
+
+    if (diffUp < diffDown)
+        idx += 1; //if closer to upper index choose this.
+
+    return idx;
+}
+
+
+
+// FUNCTION SCOPE: Is to test the class : f_kx_operators
+// This wrapper function exposes the class's constructor in a C-style fashion
+// for testing through interfacing with python (ctypes)!
+void testClassWrapperFor_f_kx_operators(float * k, float * kx, fcomp * values, \
+    int Nk, float kmax, int Nkx, float dz, const char type){
+    
+    //construct class : here we test the constructor
+    f_kx_operators ops(Nk, kmax, Nkx, dz, type, kx);
+
+    //now just copy data from class members to equivalent arrays for testing
+    //---------------------------------------
+    for(int i=0; i<Nk; ++i)
+        k[i] = ops.k[i];
+    
+    for(int i=0; i<Nkx; ++i)
+        kx[i] = ops.kx[i];
+    
+    for(int i=0; i<Nk; ++i)
+        for(int j=0; j<Nkx; ++j)
+            values[i*Nkx + j] = ops.values[i*Nkx + j];
+    //---------------------------------------
+    std::cout << std::endl;
+}
+
+} // end extern "C"
---
+++ cpp_src/prepOps.h
@@ -0,0 +1,69 @@
+#ifndef PREPOPS_H
+#define PREPOPS_H
+
+#include "types.h"
+#include <iostream>
+#include <vector>
+
+extern "C"
+
+{
+
+// Functions declarations : these functions are used inside the class: f_kx_operators.
+// The reason for NOT implementing them as member functions is for testing 
+// purposes, since the methodology relies on interfacing with python (ctypes)! 
+void create_k(float * , float , int );
+void read_kx(float * , int , float * );
+void forwOperators(fcomp * , int , float * , int , float * , float );
+void backOperators(fcomp * , int , float * , int , float * , float );
+int chooseOperatorIndex(int , float * , float );
+
+// CLASS SCOPE: prepare a 2D array of propagation operators based on a range of
+// kappa's (k=omega/velocity) and wavenumbers (kx=1/x).
+class f_kx_operators {
+
+    public:
+    
+        int Nk;
+        float kmax;
+        int Nkx;
+        float dz;
+        const char type; // should be f (for forward) or b (for backward),
+        // to specify the direction of propagation
+
+        std::vector<float> k;
+        std::vector<float> kx;
+        std::vector<fcomp> values;
+
+        // constructor
+        f_kx_operators(int Nk, float kmax, int Nkx, \
+            float dz, const char type, float * kx_from)
+                : Nk(Nk), kmax(kmax), Nkx(Nkx), dz(dz), type(type) {
+            
+            k.reserve( Nk );
+            create_k(k.data(), kmax, Nk);
+
+            kx.reserve( Nkx );
+            read_kx(kx.data(), Nkx, &kx_from[0]);
+
+            values.reserve( Nk * Nkx );
+            if (type == 'f'){
+                forwOperators(values.data(), Nk, k.data(), Nkx, kx.data(), dz);
+            } else 
+            if (type == 'b') {
+                backOperators(values.data(), Nk, k.data(), Nkx, kx.data(), dz);
+            } else {
+                
+                //set all values to zero (needed for testing purposes)
+                for(int i=0; i<Nk; ++i)
+                    for(int j=0; j<Nkx; ++j)
+                        values[i*Nkx + j] = fcomp(0.0,0.0);
+
+                std::cout << "Wrong operator type!\n";
+            }
+        }
+};
+
+} // end extern "C"
+
+#endif
